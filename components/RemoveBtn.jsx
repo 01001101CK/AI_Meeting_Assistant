697b5006086f2dacaf0f47de1a32960bbdad6d05
Yufa <112290188+01001101CK@users.noreply.github.com>
2023-10-24 20:09:01 -0700

    set up get to show all existing summary and set up delete functionality in DeleteBtn

--- components/RemoveBtn.jsx
+++ components/RemoveBtn.jsx
@@ -1,8 +1,25 @@
+"use client"
 import { HiOutlineTrash } from "react-icons/hi";
+import { useRouter } from "next/navigation";
 
-export default function RemoveBtn () {
+export default function RemoveBtn ({id}) {
+    const router = useRouter();
+    const removeSummary = async () => {
+        const confirmed = confirm("Are you sure to delete your meeting record?");
+
+        if (confirmed) {
+            const res = await fetch(`http://localhost:3000/api/summaries?id=${id}`, {
+                method: "DELETE",
+            });
+
+            if (res.ok) {
+                router.refresh();
+            }
+        }
+    };
+
     return (
-        <button className="text-red-600">
+        <button onClick={removeSummary} className="text-red-600">
             <HiOutlineTrash size={25} />
         </button>
     );
--- components/SummaryList.jsx
+++ components/SummaryList.jsx
@@ -1,20 +1,42 @@
 import RemoveBtn from "./RemoveBtn";
 
+const getSummaries = async () => {
+    try {
+        const res = await fetch("http://localhost:3000/api/summaries", {
+            cache: "no-store",
+        });
 
-export default function SummaryList () {
+        if (!res.ok) {
+            throw new Error("Failed to fetch summaries");
+        }
+
+        return res.json();
+    } catch (error) {
+        console.log("Error loading summaries: ", error);
+    }
+};
+
+export default async function SummaryList () {
+    const { summaries } = await getSummaries();
+
 return (
-<div className="p-4 border border-violet-600 my-3 flex justify-between gap-10 items-start">
-                    <div>
-                        <h2 className="font-bold text-2xl">Meeting Title</h2>
-                        <div>
-                            <h4>Meeting Summary</h4>
-                        </div>
-                    </div>
-
-                    <div className="flex gap-2">
-                        <RemoveBtn />
-                    </div>
+    <>
+        {summaries.map((summary) => (
+            <div
+                key={summary._id}
+                className="p-4 border border-violet-600 my-3 flex justify-between gap-10 items-start"
+            >
+                <div>
+                    <h2 className="font-bold text-2xl">{summary.meeting_title}</h2>
+                    <div>{summary.summary}</div>
                 </div>
+
+                <div className="flex gap-2">
+                    <RemoveBtn id={summary._id} />
+                </div>
+            </div>
+        ))}
+    </>
 )
 
 }
